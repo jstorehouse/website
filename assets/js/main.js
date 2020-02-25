6887f9d8226af4870e81cb4b29e26748ea0dd851
jstorehouse <60528251+jstorehouse@users.noreply.github.com>
2020-02-24 20:47:47 -0600

    Changed "Daily" to "Weekly" in description

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,7 +114,7 @@ function openArchive() {
 function openMessageOfWeek() {
 	document.getElementById("pick-podcast-method").style.height = "100%";
 	document.getElementById("overlay-text").innerHTML = "Message of the Week";
-	document.getElementById("overlay-description").innerHTML = "Daily audio from Joseph's Storehouses";
+	document.getElementById("overlay-description").innerHTML = "Weekly audio from Joseph's Storehouses";
 	document.getElementById("overlay-image").src="images/thumbs/message-of-week.jpg";
 	document.getElementById("apple-podcast-link").href="https://podcasts.apple.com/us/podcast/message-of-the-day/id1496234101";
 	document.getElementById("google-podcast-link").href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8xMmZkMTk2OC9wb2RjYXN0L3Jzcw==";
